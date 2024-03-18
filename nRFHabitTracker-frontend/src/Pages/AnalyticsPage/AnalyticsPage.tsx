2a1f57af5b8ae1c2033e311e0bf4e3b5e4b67360
runarli <runarli@stud.ntnu.no>
2024-03-18 16:01:47 +0100

    [#57] feat: Add all fields

--- nRFHabitTracker-frontend/src/Pages/AnalyticsPage/AnalyticsPage.tsx
+++ nRFHabitTracker-frontend/src/Pages/AnalyticsPage/AnalyticsPage.tsx
@@ -11,10 +11,20 @@ import {
   CardFooter,
 } from '@/Components/shadcnComponents/card'
 import { useLocation } from 'react-router-dom'
-import { Popover, PopoverContent, PopoverTrigger } from '@/Components/shadcnComponents/popover'
+import { Popover, PopoverContent, PopoverTrigger, PopoverClose } from '@/Components/shadcnComponents/popover'
 import { Label } from '@/Components/shadcnComponents/label'
 import { Input } from '@/Components/shadcnComponents/input'
 import { useState } from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/Components/shadcnComponents/select'
+
+// TODO: Add validation for fields
 
 export default function AnalyticsPage() {
   // Get the current location
@@ -23,19 +33,49 @@ export default function AnalyticsPage() {
   // Destructure the 'name' from the location state
   const { name } = location.state as { name: string }
 
-  // State to toggle visibility of the "Goal" card
-  const [showGoalCard, setShowGoalCard] = useState(false)
+  // Temp variable for holding information about whether there exist a goal
+  const [goalExist, setgoalExist] = useState<boolean>(false)
 
-  // State to toggle visibility of the "Add Goal" button
-  const [showGoalButton, setshowGoalButton] = useState(true)
+  const [question, setQuestion] = useState<string>('')
+  const [target, setTarget] = useState<string>('')
+  const [unit, setUnit] = useState<string>('')
+  const [frequency, setFrequency] = useState<string>('')
+  const [validInputs, setValidInputs] = useState<boolean>(false)
 
-  // State to toggle visibility of the popover
-  const [showPopover, setShowPopover] = useState(true)
+  // Function to check if all input fields are filled
+  const validateInputs = () => {
+    if (question && target && unit && frequency) {
+      setValidInputs(true)
+    } else {
+      setValidInputs(false)
+    }
+  }
+
+  // Handlers for input changes
+  const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value)
+    validateInputs()
+  }
+
+  const handleTargetChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTarget(e.target.value)
+    validateInputs()
+  }
+
+  const handleUnitChange = (value: string) => {
+    setUnit(value)
+    validateInputs()
+  }
+
+  const handleFrequencyChange = (value: string) => {
+    setFrequency(value)
+    validateInputs()
+  }
 
   const handleAddGoal = () => {
-    setShowGoalCard(true)
-    setshowGoalButton(false)
-    setShowPopover(false)
+    if (validInputs) {
+      setgoalExist(true)
+    }
   }
 
   const handleEditGoal = () => {
@@ -43,12 +83,7 @@ export default function AnalyticsPage() {
   }
 
   const handleDeleteGoal = () => {
-    setShowGoalCard(false)
-    setshowGoalButton(true)
-  }
-
-  const handleShowPopover = () => {
-    setShowPopover(true)
+    setgoalExist(false)
   }
 
   return (
@@ -58,11 +93,9 @@ export default function AnalyticsPage() {
         <h1 className="text-4xl font-bold leading-tight text-slate-900">{name}</h1>
         <Popover>
           <PopoverTrigger>
-            <Button className={`${showGoalButton ? 'visible' : 'hidden'}`} onClick={handleShowPopover}>
-              Add Goal
-            </Button>
+            <Button className={`${goalExist ? 'hidden' : 'visible'}`}>Add Goal</Button>
           </PopoverTrigger>
-          <PopoverContent className={`${showPopover ? 'visible' : 'hidden'}`}>
+          <PopoverContent>
             <div className="grid gap-4">
               <div className="space-y-2">
                 <h4 className="font-medium leading-none">Add a Goal</h4>
@@ -71,26 +104,65 @@ export default function AnalyticsPage() {
               <div className="grid gap-2">
                 <div className="grid grid-rows-2 items-center">
                   <Label htmlFor="question">Question</Label>
-                  <Input id="question" placeholder="e.g. How many hours did I sleep?" className="col-span-2 h-8" />
+                  <Input
+                    id="question"
+                    placeholder="e.g. How many hours did I sleep?"
+                    className={`col-span-2 h-8`}
+                    value={question}
+                    onChange={handleQuestionChange}
+                  />
                 </div>
                 <div className="grid grid-rows-2 items-center">
                   <Label htmlFor="target">Target</Label>
-                  <Input id="target" placeholder="e.g. 10" className="col-span-2 h-8" />
+                  <Input
+                    id="target"
+                    placeholder="e.g. 10"
+                    className={`col-span-2 h-8`}
+                    value={target}
+                    onChange={handleTargetChange}
+                  />
                 </div>
                 <div className="grid grid-rows-2 items-center">
                   <Label htmlFor="unit">Unit</Label>
-                  <Input id="unit" placeholder="e.g. Hours" className="col-span-2 h-8" />
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a unit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="hours">Hours</SelectItem>
+                        <SelectItem value="pcs">Pcs</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
                 </div>
-                <Button variant="secondary" onClick={handleAddGoal}>
-                  Add
-                </Button>
+                <div className="grid grid-rows-2 items-center">
+                  <Label htmlFor="frequency">Frequency</Label>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a frequency" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="day">Every Day</SelectItem>
+                        <SelectItem value="week">Every Week</SelectItem>
+                        <SelectItem value="month">Every Month</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <PopoverClose>
+                  <Button variant="secondary" onClick={handleAddGoal} className="w-full" disabled={!validInputs}>
+                    Add
+                  </Button>
+                </PopoverClose>
               </div>
             </div>
           </PopoverContent>
         </Popover>
       </div>
       {/* Card for displaying history chart */}
-      <Card className={`w-[100%] mx-auto ${showGoalCard ? '' : 'hidden'}`}>
+      <Card className={`w-[100%] mx-auto ${goalExist ? 'visible' : 'hidden'}`}>
         <CardHeader>
           <CardTitle>Goal</CardTitle>
           <CardDescription>Your goal for this habit</CardDescription>
@@ -115,6 +187,24 @@ export default function AnalyticsPage() {
         </CardContent>
       </Card>
       {/* Example card */}
+      <Card className="w-[100%] mx-auto">
+        <CardHeader>
+          <CardTitle>Example card</CardTitle>
+        </CardHeader>
+        <CardContent>Just to show what is possible and how it will look</CardContent>
+      </Card>
+      <Card className="w-[100%] mx-auto">
+        <CardHeader>
+          <CardTitle>Example card</CardTitle>
+        </CardHeader>
+        <CardContent>Just to show what is possible and how it will look</CardContent>
+      </Card>
+      <Card className="w-[100%] mx-auto">
+        <CardHeader>
+          <CardTitle>Example card</CardTitle>
+        </CardHeader>
+        <CardContent>Just to show what is possible and how it will look</CardContent>
+      </Card>
       <Card className="w-[100%] mx-auto">
         <CardHeader>
           <CardTitle>Example card</CardTitle>
