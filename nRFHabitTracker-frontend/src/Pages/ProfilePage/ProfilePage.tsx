d8c857cac025ed7de3b2086eeb48311f79d28819
runarli <runarli@stud.ntnu.no>
2024-04-15 14:08:58 +0200

    [#76] feat: Editable profile page using tabs

--- nRFHabitTracker-frontend/src/Pages/ProfilePage/ProfilePage.tsx
+++ nRFHabitTracker-frontend/src/Pages/ProfilePage/ProfilePage.tsx
@@ -1,20 +1,36 @@
-import { useNavigate } from 'react-router-dom'
-import { FaEnvelope, FaUser, FaLock, FaUserEdit, FaRegUser } from 'react-icons/fa'
+// Page for updating user information
+
+import { Button } from '@/Components/shadcnComponents/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/Components/shadcnComponents/card'
+import { Input } from '@/Components/shadcnComponents/input'
+import { Label } from '@/Components/shadcnComponents/label'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/Components/shadcnComponents/tabs'
 import { UserInformation, getUserInformation } from '@/Api/api'
 import { useEffect, useState } from 'react'
 
 export function ProfilePage() {
-  const navigate = useNavigate()
-  const [userData, setUserData] = useState<UserInformation | null>(null) // State to store user data
+  // State to store user data
+  const [userData, setUserData] = useState<UserInformation | null>(null)
+  // State to store username
+  const [username, setUsername] = useState<string>('')
+  // State to store email
+  const [email, setEmail] = useState<string>('')
 
   useEffect(() => {
     // Fetch user information when the component mounts
     const fetchUserData = async () => {
       try {
-        const userInfo = await getUserInformation('0') // Replace 'userId' with the actual user ID
+        const userInfo = await getUserInformation('0') // TODO: replace '0' with the actual user ID
         setUserData(userInfo)
       } catch (error) {
-        // Handle errors
+        // TODO: Handle errors
         console.error('Error fetching user information:', error)
       }
     }
@@ -22,50 +38,94 @@ export function ProfilePage() {
     fetchUserData() // Call the function to fetch user information
   }, []) // Empty dependency array ensures useEffect runs only once
 
-  if (!userData) {
-    return <div>Loading...</div> // Placeholder for when data is being fetched
+  useEffect(() => {
+    // Update local state when userData changes
+    if (userData) {
+      setUsername(userData.username)
+      setEmail(userData.email)
+    }
+  }, [userData])
+
+  // Function to handle username change
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value)
+  }
+
+  // Function to handle email change
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
   }
 
   return (
-    <div className="m-5">
-      {/* Heading and Edit Profile button */}
-      <div className="flex justify-between items-center mb-10">
-        <h1 className="text-4xl font-bold leading-tight text-slate-900">My Profile</h1>
-        <FaUserEdit size={30} className="text-gray-500" onClick={() => navigate('/edit-profile')} />
-      </div>
-      {/* Profile picture */}
-      <div className="flex justify-center">
-        <div className={`w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center`}>
-          {userData.profilePicture ? (
-            <img src={userData.profilePicture} alt="Profile" className="w-full h-full rounded-full object-cover" />
-          ) : (
-            <FaRegUser size={40} />
-          )}
-        </div>
-      </div>
-      {/* Profile fields */}
-      <div className="flex justify-center h-auto flex-wrap mt-10">
-        <div className="w-full max-w-lg flex items-center">
-          <FaEnvelope className="mr-2 text-gray-500" />
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-            {userData.email}
-          </label>
-        </div>
-        <div className="w-full max-w-lg border-b border-gray-300 my-2"></div>
-        <div className="w-full max-w-lg flex items-center mt-2">
-          <FaUser className="mr-2 text-gray-500" />
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-            {userData.username}
-          </label>
-        </div>
-        <div className="w-full max-w-lg border-b border-gray-300 my-2"></div>
-        <div className="w-full max-w-lg flex items-center mt-2">
-          <FaLock className="mr-2 text-gray-500" />
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-            {userData.password}
-          </label>
-        </div>
-      </div>
+    <div className="m-5 flex justify-center items-center">
+      {/* Tabs component for switching between account and password sections */}
+      <Tabs defaultValue="account" className="w-[30%] mx-auto" style={{ minWidth: '350px' }}>
+        {/* TabsList to display tab triggers */}
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        {/* TabsContent for the account section */}
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>Account</CardTitle>
+              <CardDescription>Make changes to your account here. Click save when you are done.</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              {/* Input for username */}
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  placeholder={userData ? userData.username : 'Trouble fetching your information...'}
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              {/* Input for email */}
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  placeholder={userData ? userData.email : 'Trouble fetching your information...'}
+                  onChange={handleEmailChange}
+                />
+              </div>
+            </CardContent>
+            {/* Button to save changes */}
+            <CardFooter>
+              {/* TODO: Add functionality when clicking button */}
+              <Button>Save changes</Button>
+            </CardFooter>
+          </Card>
+          {/* TabsContent for the password section */}
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>Change your password here.</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              {/* Input for current password */}
+              <div className="space-y-1">
+                <Label htmlFor="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              {/* Input for new password */}
+              <div className="space-y-1">
+                <Label htmlFor="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </CardContent>
+            {/* Button to save password */}
+            <CardFooter>
+              {/* TODO: Add functionality when clicking button */}
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
