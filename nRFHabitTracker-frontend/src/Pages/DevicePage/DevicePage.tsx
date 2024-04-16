153520239a036d4d52a0b09fc42e95a70de34e97
runarli <runarli@stud.ntnu.no>
2024-04-16 16:24:16 +0200

    [#84] feat: Create mock data

--- nRFHabitTracker-frontend/src/Pages/DevicePage/DevicePage.tsx
+++ nRFHabitTracker-frontend/src/Pages/DevicePage/DevicePage.tsx
@@ -2,11 +2,41 @@
 
 import { LoadingSpinner } from '@/Components/LoadingSpinner/LoadingSpinner'
 import { Button } from '@/Components/shadcnComponents/button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+
+// Interface representing the structure of a device object
+export interface Device {
+  '1': number
+  '2': number
+  '3': number
+  '4': number
+  '5': number
+  '6': number
+  '8': number
+  '9': number
+  '10': number
+  '11': number
+}
 
 export default function DevicePage() {
   const navigate = useNavigate()
+
+  const [deviceData, setDeviceData] = useState<Device[]>([])
+
+  // Mockup data
+  const mockupData: Device = {
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 1713186340194,
+    '5': 1712832241928,
+    '6': 0,
+    '8': 0,
+    '9': 1712846856329,
+    '10': 0,
+    '11': 0,
+  }
 
   // State to track saving process
   const [loading, setLoading] = useState(false)
@@ -24,6 +54,11 @@ export default function DevicePage() {
   function disconnect() {
     // TODO: Add functionality
   }
+
+  useEffect(() => {
+    // Set data when component mounts
+    setDeviceData([mockupData])
+  }, []) // Empty dependency array ensures this effect runs only once on mount
 
   return (
     <div className="flex flex-col m-5">
