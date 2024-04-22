f2ef680f390ed0bc040a3f9d68df714042b33a25
runarli <runarli@stud.ntnu.no>
2024-04-22 14:41:47 +0200

    [#86] feat: Add header to profile page

--- nRFHabitTracker-frontend/src/Pages/ProfilePage/ProfilePage.tsx
+++ nRFHabitTracker-frontend/src/Pages/ProfilePage/ProfilePage.tsx
@@ -56,76 +56,95 @@ export function ProfilePage() {
     setEmail(event.target.value)
   }
 
+  function saveChanges() {
+    // TODO: Add functionality for saving changes to the account
+  }
+
+  function savePassword() {
+    // TODO: Add functionality for saving password changes
+  }
+
+  function signOut() {
+    // TODO: Add functionality for signing out
+  }
+
   return (
-    <div className="flex justify-center items-center" style={{ height: 'calc(100vh - 112px)', overflow: 'auto' }}>
-      {/* Tabs component for switching between account and password sections */}
-      <Tabs defaultValue="account" className="w-[30%] mx-auto" style={{ minWidth: '350px' }}>
-        {/* TabsList to display tab triggers */}
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        {/* TabsContent for the account section */}
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>Make changes to your account here. Click save when you are done.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              {/* Input for username */}
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input
-                  id="username"
-                  placeholder={userData ? userData.username : 'Trouble fetching your information...'}
-                  onChange={handleUsernameChange}
-                />
-              </div>
-              {/* Input for email */}
-              <div className="space-y-1">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  placeholder={userData ? userData.email : 'Trouble fetching your information...'}
-                  onChange={handleEmailChange}
-                />
-              </div>
-            </CardContent>
-            {/* Button to save changes */}
-            <CardFooter>
-              {/* TODO: Add functionality when clicking button */}
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
-          {/* TabsContent for the password section */}
-        </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription>Change your password here.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              {/* Input for current password */}
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              {/* Input for new password */}
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            {/* Button to save password */}
-            <CardFooter>
-              {/* TODO: Add functionality when clicking button */}
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
+    <div className="m-5">
+      {/* Heading and sign out button */}
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold leading-tight text-slate-900">My Account</h1>
+        <Button onClick={signOut}>Sign out</Button>
+      </div>
+      <div className="flex items-start h-auto flex-wrap mt-10">
+        {/* Tabs component for switching between account and password sections */}
+        <Tabs defaultValue="account" className="w-[30%] mx-auto" style={{ minWidth: '320px' }}>
+          {/* TabsList to display tab triggers */}
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+          {/* TabsContent for the account section */}
+          <TabsContent value="account">
+            <Card>
+              <CardHeader>
+                <CardTitle>Account</CardTitle>
+                <CardDescription>Make changes to your account here. Click save when you are done.</CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                {/* Input for username */}
+                <div className="space-y-1">
+                  <Label htmlFor="username">Username</Label>
+                  <Input
+                    id="username"
+                    placeholder={userData ? userData.username : 'Trouble fetching your information...'}
+                    onChange={handleUsernameChange}
+                  />
+                </div>
+                {/* Input for email */}
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    placeholder={userData ? userData.email : 'Trouble fetching your information...'}
+                    onChange={handleEmailChange}
+                  />
+                </div>
+              </CardContent>
+              {/* Button to save changes */}
+              <CardFooter>
+                {/* TODO: Add functionality when clicking button */}
+                <Button onClick={saveChanges}>Save changes</Button>
+              </CardFooter>
+            </Card>
+            {/* TabsContent for the password section */}
+          </TabsContent>
+          <TabsContent value="password">
+            <Card>
+              <CardHeader>
+                <CardTitle>Password</CardTitle>
+                <CardDescription>Change your password here.</CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                {/* Input for current password */}
+                <div className="space-y-1">
+                  <Label htmlFor="current">Current password</Label>
+                  <Input id="current" type="password" />
+                </div>
+                {/* Input for new password */}
+                <div className="space-y-1">
+                  <Label htmlFor="new">New password</Label>
+                  <Input id="new" type="password" />
+                </div>
+              </CardContent>
+              {/* Button to save password */}
+              <CardFooter>
+                {/* TODO: Add functionality when clicking button */}
+                <Button onClick={savePassword}>Save password</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   )
 }
