2c4c513b424621750ac90e403f0b1d85444afa18
runarli <runarli@stud.ntnu.no>
2024-04-22 15:18:39 +0200

    [#86] feat: Make navbar functional

--- nRFHabitTracker-frontend/src/Components/navBar/navBar.tsx
+++ nRFHabitTracker-frontend/src/Components/navBar/navBar.tsx
@@ -7,11 +7,45 @@ export function NavBar() {
   function goToLoginPage() {
     navigate('/')
   }
+
+  // Navigate to other pages
+  function navigateTo(page: string) {
+    navigate(page)
+  }
+
+  // Function to determine if the text should have a different color based on whether it is clicked
+  function isActive(path: string) {
+    return location.pathname === path ? '#94A3B8' : 'white'
+  }
+
   return (
-    <div className="sticky w-full h-14 bg-[#334155] flex top-0 left-0 border-b border-[#E4E4E7] z-50">
-      <img onClick={goToLoginPage} className="cursor-pointer pl-2.5 pt-0.5 pb-0.5" src="/dodIcon.png" alt="icon" />
-      <div className="flex items-center ml6 flex-grow">
-        <p className="text-white font-[Inter] text-base font-medium normal-case pl-6 ">NRF- Habit Tracker</p>
+    <div className="sticky w-full h-14 bg-[#334155] flex items-center justify-between top-0 left-0 border-b border-[#E4E4E7] z-50">
+      <div className="flex items-center">
+        <img onClick={goToLoginPage} className="cursor-pointer h-12 ml-2 mr-4" src="/dodIcon.svg" alt="dodIcon" />
+        <p className="text-white text-lg font-medium">NRF - Habit Tracker</p>
+      </div>
+      <div className="flex items-center pr-4">
+        <button
+          className="text-white text-lg font-medium mr-10"
+          style={{ color: isActive('/profile') }}
+          onClick={() => navigateTo('/profile')}
+        >
+          Account
+        </button>
+        <button
+          className="text-white text-lg font-medium mr-10"
+          style={{ color: isActive('/my-habits') }}
+          onClick={() => navigateTo('/my-habits')}
+        >
+          Habits
+        </button>
+        <button
+          className="text-white text-lg font-medium mr-2"
+          style={{ color: isActive('/my-device') }}
+          onClick={() => navigateTo('/my-device')}
+        >
+          Device
+        </button>
       </div>
     </div>
   )
